dc7b19644a305ac4e822163eb44b29f9a95b9248
Clint <55072157+clintonbf@users.noreply.github.com>
2020-11-06 12:21:12 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/3522_A02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3522_A02.exe"
+  "3522_A02.pdb"
+  "CMakeFiles/3522_A02.dir/main.cpp.o"
+  "lib3522_A02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3522_A02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
