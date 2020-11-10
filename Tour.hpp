5f715447a846ca778105f04d428c600f8ef4b372
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-11-09 21:10:27 -0800

    commented tour

--- Tour.hpp
+++ Tour.hpp
@@ -19,23 +19,41 @@ private:
     static constexpr double MUTATION_RATE = 0.15;
     vector<City> cities;
     double fitness = 0;
+
+    /**
+     * Evaluate the fitness of the tour.
+     */
     void evaluateFitness();
+
+    /**
+     * Swap the positions of two cities in the tour
+     * @param a index of a city
+     * @param b index of a city
+     */
     void swapCities( unsigned long a, unsigned long b);
+
+    /**
+    * Randomize the order of the tour
+    */
+    void randomizeOrder();
 public:
     explicit Tour(vector<City> &newCities): cities(newCities){
         randomizeOrder();
         evaluateFitness();
     }
 
-    void mutate(); //Todo: implementation
-
-    void randomizeOrder(); //Todo: implementation
+    /**
+     * Slightly change the order of the cities in tour.
+     *
+     * There is a 0.15^n chance the order will not change.
+     */
+    void mutate();
 
     /**
      * Add a City to the Tour.
      * @param c City& City to add
      */
-    void addCity(City& c); //Todo: implementation
+    void addCity(City& c);
 
     /**
      * Get if a tour contains a City.
@@ -43,10 +61,20 @@ public:
      * @param city City& the City to search for
      * @return boolean
      */
-    bool containsCity(const City& city); //Todo: implementation
+    bool containsCity(const City& city);
 
+    /**
+     * Get the cities in the tour.
+     * @return a vector of cities
+     */
     const vector<City>& getCities() const { return cities; }
 
+    /**
+     * Insert a tour into output stream
+     * @param os the output stream
+     * @param tour a tour
+     * @return the output stream, after the fact
+     */
     friend ostream &operator<<( ostream &os, const Tour &tour );
 };
 
